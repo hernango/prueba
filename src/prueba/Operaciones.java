5bc20bd3b52d624acc2a6e90724664205eaee6be
hernango <hernanhidelberg@gmail.com>
2018-03-08 15:25:33 -0500

    cree una clase operaciones con metodo para potencias

---
+++ src/prueba/Operaciones.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author Hernan_Gomez
+ */
+public class Operaciones {
+    
+    public double elevar(int base, int exp){
+    double resultado =1;
+        for(int i=1 ; i<=exp; i++)
+         resultado *=base;
+ return resultado;   
+}
+    
+}
---
+++ src/prueba/Prueba.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author Hernan_Gomez
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean desicion=true;
+     for(int i=0; desicion;i++){
+       if(i==3){desicion=false;}
+       System.out.println("SI se puede");
+       
+     }   
+    }
+    
+}
